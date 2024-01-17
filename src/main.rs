7182d94fc20b155ee7c7c60df00e01b7fdaa2dc3
Bastian Asmussen <bastian@asmussen.tech>
2024-01-17 12:09:16 +0100

    Created `seed_todo` function.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 use crate::db::model::Todo;
 use color_eyre::Result;
+use diesel::PgConnection;
 
 mod db;
 
 fn main() -> Result<()> {
     let mut conn = db::get_connection()?;
 
-    let todo = Todo::new(&mut conn, "Shopping")?;
-    let mut task = todo.add_task(&mut conn, "Get Apples")?;
-    task.complete(&mut conn)?;
+    let todo = seed_todo(&mut conn)?;
 
-    println!("Todo: {todo:?}");
-    println!("Task: {task:?}");
+    println!("{}:", todo.name);
+    todo.get_tasks(&mut conn)?
+        .iter()
+        .for_each(|task| println!("- {}", task.name));
 
     Ok(())
+}
+
+fn seed_todo(conn: &mut PgConnection) -> Result<Todo> {
+    let todo = Todo::new(conn, "Produce Software")?;
+    todo.add_task(conn, "Write Code")?;
+    todo.add_task(conn, "Compile Source")?;
+    todo.add_task(conn, "Run Tests")?;
+
+    Ok(todo)
 }
