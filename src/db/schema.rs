85b80659c23178aab69f014133f7e0a0cbcbc096
Bastian Asmussen <bastian@asmussen.tech>
2024-01-22 09:09:17 +0100

    Solved `2.3`.

--- src/db/schema.rs
+++ src/db/schema.rs
@@ -1,21 +1,33 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
-    tasks (id) {
+    teams (id) {
         id -> Int4,
+        #[max_length = 255]
         name -> Varchar,
-        is_completed -> Bool,
-        todo_id -> Int4,
     }
 }
 
 diesel::table! {
-    todos (id) {
+    teams_workers (team_id, worker_id) {
+        team_id -> Int4,
+        worker_id -> Int4,
+    }
+}
+
+diesel::table! {
+    workers (id) {
         id -> Int4,
+        #[max_length = 255]
         name -> Varchar,
     }
 }
 
-diesel::joinable!(tasks -> todos (todo_id));
+diesel::joinable!(teams_workers -> teams (team_id));
+diesel::joinable!(teams_workers -> workers (worker_id));
 
-diesel::allow_tables_to_appear_in_same_query!(tasks, todos,);
+diesel::allow_tables_to_appear_in_same_query!(
+    teams,
+    teams_workers,
+    workers,
+);
